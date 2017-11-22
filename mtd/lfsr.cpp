6671aed3b1c15a92201957b945d6eee53e09d48f
bhavan0 <bhavan0@node-2.bhavan0-qv30720.emulab-net.utahddc.geniracks.net>
2017-11-22 08:41:57 -0700

    First commit

--- mtd/lfsr.cpp
+++ mtd/lfsr.cpp
@@ -67,7 +67,7 @@ int ip_addr[24];
 char output[6];
 unsigned long int period=0;
 FILE *myfile;
-myfile=fopen("time_cputime_lfsr.text", "a");
+myfile=fopen("time_walltime_lfsrupdateonly.text", "a");
 clock_t t;
 t=clock();
 double tume_spent;
@@ -77,28 +77,50 @@ char buffer[30];
 
 //wall time
 
-struct timeval tv;
-time_t curtime;
-gettimeofday(&tv,NULL);
-curtime=tv.tv_sec;
-strftime(buffer,30,"%T.",localtime(&curtime));
+//struct timeval tv;
+//time_t curtime;
+//gettimeofday(&tv,NULL);
+//curtime=tv.tv_sec;
+//strftime(buffer,30,"%T.",localtime(&curtime));
 //printf("%s%ld\n",buffer,tv.tv_usec);
 
 
 int clockcycle=0;
 do{
 do{
+struct timeval tv;                                                                                               
+time_t curtime;                                                                                                  
+gettimeofday(&tv,NULL);                                                                                          
+curtime=tv.tv_sec;                                                                                               
+strftime(buffer,30,"%T.",localtime(&curtime));                                                                   
+printf("%s%ld\n",buffer,tv.tv_usec);                                                                             
+                                        
+
+
 unsigned lsb = lfsr &1;
 lfsr >>=1;
 
 if(lsb==1)
 lfsr ^=0xE00040u;
-sprintf(output,"%X",lfsr);
+//sprintf(output,"%X",lfsr);
 
-convertDectoHex(output,ip_addr);
+char buffer2[230];                                                                                               
+struct timeval tv2;                                                                                              
+time_t curtime2;                                                                                                 
+gettimeofday(&tv2,NULL);                                                                                         
+curtime2=tv2.tv_sec;                                                                                             
+strftime(buffer2,30,"%T.",localtime(&curtime2));                                                                 
+printf("%s%ld seconds\n",buffer2,tv2.tv_usec);  
+
+
+printf("%f\n",float(tv2.tv_usec-tv.tv_usec)/1000);
+
+fprintf(myfile,"%f seconds", float(tv2.tv_usec-tv.tv_usec)/1000);                                                                        
+fprintf(myfile,"\n");   
+//convertDectoHex(output,ip_addr);
 ++period;
 }
-while(lfsr !=start_state && period!=10000);
+while(lfsr !=start_state && period!=1);
 printf("period cycle %d : %lu \n",clockcycle+1,period);
 period=0;
 clockcycle ++;
@@ -107,18 +129,18 @@ while(clockcycle!=1);
 
 t=clock()-t;
 double time_taken =((double)t)/CLOCKS_PER_SEC;
-fprintf(myfile,"%f seconds", time_taken);
-fprintf(myfile,"\n");
+//fprintf(myfile,"%f seconds", time_taken);
+//fprintf(myfile,"\n");
 
-char buffer2[230];
-struct timeval tv2;
-time_t curtime2;
-gettimeofday(&tv2,NULL);
-curtime2=tv2.tv_sec;
-strftime(buffer2,30,"%T.",localtime(&curtime2));
+//char buffer2[230];
+//struct timeval tv2;
+//time_t curtime2;
+//gettimeofday(&tv2,NULL);
+//curtime2=tv2.tv_sec;
+//strftime(buffer2,30,"%T.",localtime(&curtime2));
 //printf("%s%ld seconds\n",buffer2,tv2.tv_usec);
 
-printf("%f\n",float(tv2.tv_usec-tv.tv_usec)/1000);
+//printf("%f\n",float(tv2.tv_usec-tv.tv_usec)/1000);
 //fprintf(myfile,"%f seconds", float(tv2.tv_usec-tv.tv_usec)/1000);
 //fprintf(myfile,"\n");
 return 0;
